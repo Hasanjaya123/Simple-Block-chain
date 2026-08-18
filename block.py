61aa376f1f4d531a2f65f5880eef7279c99b8564
Hasanjaya123 <vibercat723@gmail.com>
2026-08-19 02:48:14 +0530

    feat: add transaction layer

--- block.py
+++ block.py
@@ -1,8 +1,65 @@
+import hashlib
+import json
 
+class MaxTransactionLimitException(Exception):
+    pass
+    
 
 class Block:
     
-    def ___init__(self, index: int, data: str):
+    def ___init__(self, transaction: Transaction):
+        
+        self.transactions = []
+        self.hash = hashlib.sha256(str(transaction).encode()).hexdigest()
+        
+    def validate_transations(self, transaction: Transaction):
+        
+        if self.transactions >= 10:
+            self.transactions.append(transaction)
+        
+        else:
+            raise MaxTransactionLimitException("Only Maximum of 10 Transactions can be added to an oneblock")
+            
+
+    def __repr__(self):
+        return f"{self.sender} sent {self.reciver} Rs.{self.amount}"
+    
+    
+class Transaction():
+    
+    def __init__(self, index: int, sender: Person, receiver: Person, amount: float):
         
         self.index = index
-        self.data = data
+        self.sender = sender
+        self.reciver = receiver
+        self.amount = amount
+        
+    def __str__(self): 
+            
+        transaction = {
+                    "Index" : self.index,
+                    "Sender's Name": self.sender,
+                    "Reciver's Name": self.reciver,
+                    "Amount": f"Rs.{self.amount}"
+                }
+        return json.dumps(transaction, indent=4)
+        
+    def __repr__(self): 
+        
+        transaction = {
+                    "Index" : self.index,
+                    "Sender's Name": self.sender,
+                    "Reciver's Name": self.reciver,
+                    "Amount": f"Rs.{self.amount}"
+                }
+        return json.dumps(transaction, indent=4)
+        
+        
+class Person:
+    
+    def __init__(self, name:str, age: int):
+        
+        self.name = name
+        self.age = age
+        
+        
--- block_chain.py
+++ block_chain.py
@@ -1,4 +1,5 @@
 from block import Block
+from block import Person
 
 class BlockChainException(Exception):
     pass
@@ -8,12 +9,13 @@ class BlockChain:
     def __init__(self):
         
         genesis = Block(0, "Genesis Block")
-        self.chain = [genesis]
+        self.chain = []
+        self.chain.append(genesis)
         
-    def add_block(self, data):
+    def add_block(self, sender: Person, receiver: Person, amount: float):
         
         last_block = self.get_last_block()
-        block = Block(self.get_last_block().index +1, data)
+        block = Block(last_block.index +1, sender, receiver, amount)
         
         self.chain.append(block)
     
@@ -21,5 +23,11 @@ class BlockChain:
         
         return self.chain[-1]
     
+    
+kamal = Person("Kamal", 22)
+nimal = Person("nimal", 24)
+    
 coin = BlockChain()
+
+coin.add_block(kamal, nimal, 100.00)
 
